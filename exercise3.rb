c842a683b614b3ce4d4554d090aec2f29184c5ef
Victor McLean <vmvictorvm@gmail.com>
2016-08-09 14:28:36 -0400

    exercise3 added year of birth

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,9 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}"
+
+#Last step, Ask user how old they are
+puts "How old are you?"
+age = gets.chomp
+yearOfBirth = 2016 - age.to_i
+puts "Your were born in #{yearOfBirth}"
